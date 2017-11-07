443ace077a228e71113f74f0034e4072eda57c56
carebear19 <33457257+carebear19@users.noreply.github.com>
2017-11-07 09:43:18 -0500

    First upload

---
+++ Personality_survey_CH.py
@@ -0,0 +1,45 @@
+print ("What's your favorite sport")
+sport = input()
+
+if sport == "softball":
+    print("That's my favorite sport too!")
+elif sport == "Basketball":
+    print("Basketball is my other favorite sport too!")
+elif sport == "Field Hockey":
+    print("Whoah I love Field Hockey! You do to? Awesome!")
+else:
+    print(sport + " sounds fun to play.")
+
+
+
+print("What's your favorite subject")
+subject = input()
+
+if subject == "Math":
+    print("Zero product property is my personal favorite.")
+elif subject == "Art":
+    print("Oh, I enjoy drawing still life prottraits")
+elif subject == "English":
+    print("In class I am reading The Sun is Also A Star")
+else:
+    print(subject + " is a great class too.")
+
+
+print("What is your favorite school activity")
+activity = input()
+
+if activity == "Field trips":
+    print("I enjoy going on trips with my school as well!")
+
+print("What's your favorite food?")
+food = input()
+
+if food == "pizza":
+    print("What's your favorite kind")
+    favkind = input()
+    if favkind == "white cheese":
+        print("Yum!")
+    else:
+            print("That's nice.")
+elif food == "orange":
+    print("Orange ya glad I didn't say banana?")
